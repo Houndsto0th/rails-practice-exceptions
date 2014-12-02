8b72a3e783a0941f681b1bb3ef832c0055997da8
Houndsto0th <bry.kinnear@gmail.com>
2014-12-02 11:44:34 -0700

    Added specs and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class AccessDenied < StandardError
+  end
+
+  rescue_from AccessDenied, with: :unfound
+
+  before_action :ensure_logged_in
+
+  def ensure_logged_in
+    if session[:user_id].nil?
+      redirect_to signin_path
+    end
+  end
+
+  def unfound
+    render "public/404", status: :not_found, layout: false
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,18 @@
 class PagesController < ApplicationController
 
+  before_action :authorize
+
+  def index
+  end
+
+
+  private
+
+  def authorize
+    if session[:user_id] != 3
+      raise AccessDenied
+    end
+  end
+
+
 end
